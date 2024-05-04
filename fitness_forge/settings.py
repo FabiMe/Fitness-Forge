916836e9fe392deb7ba2a32c580a93e16a59a110
Fabian Meister <fabimeister@gmx.de>
2024-05-04 20:13:54 +0000

    added bucket config

--- fitness_forge/settings.py
+++ fitness_forge/settings.py
@@ -185,8 +185,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'fitness-forge'
+    AWS_STORAGE_BUCKET_NAME = 'fitness-forge-bucket'
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
