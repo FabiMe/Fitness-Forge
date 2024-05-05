fc65f6e26ec74b9f3ac988d7aea4a637ff0ac192
Fabian Meister <fabimeister@gmx.de>
2024-05-05 01:08:38 +0000

    customize sign up templates3

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import UserProfile
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class UserProfileForm(forms.ModelForm):
